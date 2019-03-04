2c12e36a31b2b1193422b17d408ff663d72a1cd3
s00190970 <s00190970@mail.itsligo.ie>
2019-03-04 21:29:26 +0000

    * Added Comments class

---
+++ StudentAssAttSys.Core/Core/Comment.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StudentAssAttSys.Core.Core
+{
+    public class Comment
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Message { get; set; }
+        [ForeignKey("Result")]
+        public int ResultId { get; set; }
+
+        public virtual Result Result { get; set; }
+
+    }
+}
