513a903ed70e13b1b5f0e61d11d606be6c2e5680
Jonathan <jpierre@suid.fr>
2019-03-04 21:59:36 +0000

    Add Foreign Key in Comment Class for user

--- StudentAssAttSys.Core/Core/Comment.cs
+++ StudentAssAttSys.Core/Core/Comment.cs
@@ -14,6 +14,8 @@ namespace StudentAssAttSys.Core.Core
         public DateTime DateTimeCreation { get; set; }
         [ForeignKey("Result")]
         public int ResultId { get; set; }
+        [ForeignKey("User")]
+        public string UserId { get; set; }
 
         public virtual Result Result { get; set; }
         public virtual User User { get; set; }
