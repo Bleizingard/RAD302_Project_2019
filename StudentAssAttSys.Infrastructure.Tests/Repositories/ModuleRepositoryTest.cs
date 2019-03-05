966e02a8dc2b832b5c1aca248c3a614bb1880658
Jonathan <jpierre@suid.fr>
2019-03-05 01:22:50 +0000

    Test Architecture Project Added

---
+++ StudentAssAttSys.Infrastructure.Tests/Repositories/ModuleRepositoryTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using NUnit.Framework;
+using StudentAssAttSys.Infrastructure.Repositories;
+
+namespace StudentAssAttSys.Infrastructure.Tests.Repositories
+{
+    [TestFixture]
+    public class ModuleRepositoryTest
+    {
+        private ModuleRepository Repository = new ModuleRepository();
+
+        [Test]
+        public void ShouldAddModule()
+        {
+        }
+
+        [Test]
+        public void ShouldEditModule()
+        {
+        }
+
+        [Test]
+        public void ShouldDeleteModule()
+        {
+        }
+
+        [Test]
+        public void ShouldGetModuleById()
+        {
+        }
+
+        [Test]
+        public void ShouldGetAllModule()
+        {
+        }
+
+        [Test]
+        public void ShouldThrowNotImplementedError()
+        {
+            Assert.Throws<NotImplementedException>(() => Repository.GetById(string.Empty));
+        }
+    }
+}
