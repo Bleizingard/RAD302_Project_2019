f01ab180ceef02b63a176be107fd5893eefca0d7
Jonathan <jpierre@suid.fr>
2019-03-05 01:04:39 +0000

    Repository Folder Added
    IGenericRepository Added
    ModuleRepository Added

---
+++ StudentAssAttSys.Core/IRepositories/IGenericRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentAssAttSys.Core.IRepositories
+{
+    public interface IGenericRepository<T>
+    {
+        T GetById(int id);
+        T GetById(string id);
+        bool Add(T o);
+        bool Edit(T o);
+        bool Remove(T o);
+        List<T> GetAll();
+    }
+}
---
+++ StudentAssAttSys.Infrastructure/Repositories/ModuleRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StudentAssAttSys.Core.Core;
+using StudentAssAttSys.Core.IRepositories;
+
+namespace StudentAssAttSys.Infrastructure.Repositories
+{
+    public class ModuleRepository : IGenericRepository<Module>
+    {
+        public bool Add(Module o)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Edit(Module o)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Module> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Module GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Module GetById(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(Module o)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
