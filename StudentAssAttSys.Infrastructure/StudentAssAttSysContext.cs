5bcf67839948952a6a3ab0487238d7ac93bbf15e
Jonathan <jpierre@suid.fr>
2019-03-05 00:47:29 +0000

    DbContext Fill & Configured

--- StudentAssAttSys.Infrastructure/StudentAssAttSysContext.cs
+++ StudentAssAttSys.Infrastructure/StudentAssAttSysContext.cs
@@ -3,6 +3,7 @@ namespace StudentAssAttSys.Infrastructure
     using System;
     using System.Data.Entity;
     using System.Linq;
+    using StudentAssAttSys.Core.Core;
 
     public class StudentAssAttSysContext : DbContext
     {
@@ -15,11 +16,25 @@ namespace StudentAssAttSys.Infrastructure
         public StudentAssAttSysContext()
             : base("name=StudentAssAttSysContext")
         {
+            //Configuring DbContext
+            this.Configuration.LazyLoadingEnabled = false;
+
+            //Create DatabaseIfNotExist Initializer
+            Database.SetInitializer<StudentAssAttSysContext>(new DropCreateDatabaseIfModelChanges<StudentAssAttSysContext>());
         }
 
         // Add a DbSet for each entity type that you want to include in your model. For more information 
         // on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
 
         // public virtual DbSet<MyEntity> MyEntities { get; set; }
+        public virtual DbSet<Student> Students { get; set; }
+        public virtual DbSet<Lecturer> Lecturers { get; set; }
+        public virtual DbSet<Assessment> Assessments { get; set; }
+        public virtual DbSet<Attendance> Attendances { get; set; }
+        public virtual DbSet<ClassGroup> ClassGroups { get; set; }
+        public virtual DbSet<Comment> Comments  { get; set; }
+        public virtual DbSet<Module> Modules { get; set; }
+        public virtual DbSet<Result> Results { get; set; }
+
     }
 }
