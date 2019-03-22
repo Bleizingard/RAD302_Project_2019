01da4b1bded0c2b9ea5377943b22cf0cf80d2319
s00190970 <s00190970@mail.itsligo.ie>
2019-03-22 15:30:57 +0000

    Assessments controller

---
+++ StudentAssAttSys.Infrastructure/Repositories/AssessmentRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StudentAssAttSys.Core.Core;
+using StudentAssAttSys.Core.IRepositories;
+
+namespace StudentAssAttSys.Infrastructure.Repositories
+{
+    public class AssessmentRepository : IGenericRepository<Assessment, int>
+    {
+        public int Add(Assessment o)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Edit(Assessment o)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Assessment[] GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Assessment GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(Assessment o)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
