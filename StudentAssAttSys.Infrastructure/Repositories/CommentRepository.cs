9fc046c04a62cbfac2cc9664b86b3ba4cc98881d
s00190970 <s00190970@mail.itsligo.ie>
2019-03-22 17:14:21 +0000

    Comments Repository + controller

---
+++ StudentAssAttSys.Infrastructure/Repositories/CommentRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StudentAssAttSys.Core.Core;
+using StudentAssAttSys.Core.IRepositories;
+
+namespace StudentAssAttSys.Infrastructure.Repositories
+{
+    public class CommentRepository : IGenericRepository<Comment, int>
+    {
+        public int Add(Comment o)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Edit(Comment o)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Comment[] GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Comment GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(Comment o)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
