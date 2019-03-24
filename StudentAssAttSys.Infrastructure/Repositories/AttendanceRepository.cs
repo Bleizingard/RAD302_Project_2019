9a5aebf8aac01650805bbeb58b32a6b8ce57c5c7
Jonathan PIERRE <jpierre@suid.fr>
2019-03-24 19:01:19 +0100

    Repository Added

---
+++ StudentAssAttSys.Infrastructure/Repositories/AttendanceRepository.cs
@@ -0,0 +1,53 @@
+﻿using StudentAssAttSys.Core.Core;
+using StudentAssAttSys.Core.IRepositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentAssAttSys.Infrastructure.Repositories
+{
+    public class AttendanceRepository : IAttendanceRepository
+    {
+        public int Add(Attendance o)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Close(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Edit(Attendance o)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Attendance[] GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Attendance GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Open(int id, DateTime startDateTime, DateTime endDateTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Open(int id, DateTime endDateTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(Attendance o)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ StudentAssAttSys.Infrastructure/Repositories/ResultRepository.cs
@@ -0,0 +1,38 @@
+﻿using StudentAssAttSys.Core.Core;
+using StudentAssAttSys.Core.IRepositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentAssAttSys.Infrastructure.Repositories
+{
+    public class ResultRepository : IGenericRepository<Result, KeyValuePair<int, string>>
+    {
+        public KeyValuePair<int, string> Add(Result o)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Edit(Result o)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Result[] GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Result GetById(KeyValuePair<int, string> id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(Result o)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
