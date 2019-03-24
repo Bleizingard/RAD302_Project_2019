625e8db24991f911a7917e646b3f632203f17446
Cristina Mihaila <S00184388@mail.itsligo.ie>
2019-03-24 19:00:39 +0000

    Admin Component created

---
+++ frontend/src/Components/Admin/editLecturer.js
@@ -0,0 +1,66 @@
+import React, { Component } from "react";
+import "../Admin/Admin.css";
+
+export class EditLecturer extends Component {
+  displayName = EditLecturer.name;
+  constructor(props) {
+    super(props);
+    this.state = { value: "" };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event) {
+    alert("A name was submitted: " + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    const lecturers = ["Lect 1", "Lect 2", "Lect 3", "Others"];
+    const options = lecturers.map(opt => <option key={opt}>{opt}</option>);
+    return (
+      <div>
+        <h3>Assign a new Lecturer for this module</h3>
+        <hr />
+        <form onSubmit={this.handleSubmit}>
+          <div className="row">
+            <div className="form-group input-group-sm col-sm-4">
+              <label>Module Name:</label>
+              <p>Module 1</p>
+
+              <label htmlFor="lecturers">Choose Lecturer</label>
+              <select
+                id="lecturer"
+                name="lecturer"
+                className="form-control"
+                defaultValue=""
+                onChange={this.handleChange}
+              >
+                <option value="" disabled hidden>
+                  Lecturers
+                </option>
+                {options}
+              </select>
+            </div>
+          </div>
+        </form>
+        <div className="row">
+          <button
+            type="submit"
+            className="btn btn-sm btn-primary "
+            onClick={this.onChange}
+          >
+            Save Changes
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default EditLecturer;
--- frontend/src/Components/Layout.js
+++ frontend/src/Components/Layout.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { NavMenu } from "./Navbar/NavMenu";
 
 export class Layout extends Component {
   displayName = Layout.name;
@@ -7,7 +8,7 @@ export class Layout extends Component {
     return (
       <div className="container-fluid">
         <div className="col-sm-3">
-          <h1>Navbar</h1>
+          <NavMenu />
         </div>
         <div className="col-sm-9">{this.props.children}</div>
       </div>
